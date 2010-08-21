4ce49f60f7b20159382d2051ca4a1c3731efdfa3
Angel <wage@foton.es>
2010-08-21 21:44:38 +0100

    CRUD features

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,6 +2,17 @@ class StoriesController < ApplicationController
 
   only_admins( :new, :create, :edit, :update )
 
+  def index
+  end
+
+  def show
+    @story = Story.find(params[:id])
+
+    if current_user and not current_user.admin? and @story.draft?
+      return not_found
+    end
+  end
+
   def new
     @story = Story.new
   end
@@ -18,11 +29,29 @@ class StoriesController < ApplicationController
     end
   end
 
-  def show
-    @story = Story.find(params[:id])
-
-    if current_user and not current_user.admin? and @story.draft?
+  def edit
+    begin
+      @story = Story.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
       return not_found
     end
   end
+
+  def update
+    begin
+      @story = Story.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      return not_found
+    end
+    @story.update_attributes(params[:story])
+
+    if @story.save
+      flash[:notice] = I18n.t("stories.notice_saved") 
+      redirect_to story_path(@story)
+    else
+      render :action => "edit"
+    end
+  end
+
+
 end
